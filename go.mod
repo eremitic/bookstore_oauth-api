041863d25f51a716092a8d4e3a2a1899b5e54564
elvin <eremitic.elvin@hotmail.com>
2021-03-07 19:02:38 +0800

    add cassandra

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/eremitic/bookstore_oauth-api
+
+go 1.15
+
+require github.com/gocql/gocql v0.0.0-20210303210847-f18e0979d243
--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/eremitic/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/eremitic/bookstore_oauth-api/src/domain/access_token"
 	"github.com/eremitic/bookstore_oauth-api/src/domain/utils/errors"
 )
@@ -17,5 +18,13 @@ type dbRepository struct {
 }
 
 func (r *dbRepository) GetById(string) (*access_token.AccessToken, *errors.RestErr) {
+
+	session, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+
 	return nil, errors.NewInternalErr("database no implemented")
 }
