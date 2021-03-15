9d51f2f2ecd7bc7fd859fd87aca47dc544e4a66f
elvin <eremitic.elvin@hotmail.com>
2021-03-15 20:51:12 +0800

    add access token stuff copy from complete tut, since video missing

--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -1,46 +1,83 @@
 package access_token
 
 import (
-	"github.com/eremitic/bookstore_oauth-api/src/domain/utils/errors"
+	"fmt"
+	"github.com/federicoleon/bookstore_oauth-api/src/utils/crypto_utils"
+	"github.com/federicoleon/bookstore_utils-go/rest_errors"
 	"strings"
 	"time"
 )
 
 const (
-	expirationTime = 24
+	expirationTime             = 24
+	grantTypePassword          = "password"
+	grandTypeClientCredentials = "client_credentials"
 )
+
+type AccessTokenRequest struct {
+	GrantType string `json:"grant_type"`
+	Scope     string `json:"scope"`
+
+	// Used for password grant type
+	Username string `json:"username"`
+	Password string `json:"password"`
+
+	// Used for client_credentials grant type
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+func (at *AccessTokenRequest) Validate() rest_errors.RestErr {
+	switch at.GrantType {
+	case grantTypePassword:
+		break
+
+	case grandTypeClientCredentials:
+		break
+
+	default:
+		return rest_errors.NewBadRequestError("invalid grant_type parameter")
+	}
+
+	//TODO: Validate parameters for each grant_type
+	return nil
+}
 
 type AccessToken struct {
 	AccessToken string `json:"access_token"`
 	UserId      int64  `json:"user_id"`
-	ClientId    int64  `json:"client_id"`
+	ClientId    int64  `json:"client_id,omitempty"`
 	Expires     int64  `json:"expires"`
 }
 
-func GetNewAccessToken() AccessToken {
-	return AccessToken{Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix()}
+func (at *AccessToken) Validate() rest_errors.RestErr {
+	at.AccessToken = strings.TrimSpace(at.AccessToken)
+	if at.AccessToken == "" {
+		return rest_errors.NewBadRequestError("invalid access token id")
+	}
+	if at.UserId <= 0 {
+		return rest_errors.NewBadRequestError("invalid user id")
+	}
+	if at.ClientId <= 0 {
+		return rest_errors.NewBadRequestError("invalid client id")
+	}
+	if at.Expires <= 0 {
+		return rest_errors.NewBadRequestError("invalid expiration time")
+	}
+	return nil
+}
+
+func GetNewAccessToken(userId int64) AccessToken {
+	return AccessToken{
+		UserId:  userId,
+		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
+	}
 }
 
 func (at AccessToken) IsExpired() bool {
-	now := time.Now().UTC()
-	expirationTime := time.Unix(at.Expires, 0)
-
-	return expirationTime.Before(now)
+	return time.Unix(at.Expires, 0).Before(time.Now().UTC())
 }
 
-func (at *AccessToken) Validate() *errors.RestErr {
-	at.AccessToken = strings.TrimSpace(at.AccessToken)
-	if at.AccessToken == "" {
-		return errors.NewBadReqErr("invalid token id")
-	}
-	if at.UserId <= 0 {
-		return errors.NewBadReqErr("invalid user id")
-	}
-	if at.ClientId <= 0 {
-		return errors.NewBadReqErr("invalid client id")
-	}
-	if at.Expires <= 0 {
-		return errors.NewBadReqErr("invalid expire")
-	}
-	return nil
+func (at *AccessToken) Generate() {
+	at.AccessToken = crypto_utils.GetMd5(fmt.Sprintf("at-%d-%d-ran", at.UserId, at.Expires))
 }
--- src/go.mod
+++ src/go.mod
@@ -4,7 +4,10 @@ go 1.16
 
 require (
 	github.com/eremitic/bookstore_oauth-api v0.0.0-20210307135208-b95d06d61dbe
+	github.com/federicoleon/bookstore_oauth-api v0.0.0-20200406082912-587659d634fa
+	github.com/federicoleon/bookstore_utils-go v0.0.0-20200406081703-a9b52b6e34e6
 	github.com/gin-gonic/gin v1.6.3
 	github.com/gocql/gocql v0.0.0-20210303210847-f18e0979d243
+	github.com/mercadolibre/golang-restclient v0.0.0-20170701022150-51958130a0a0
 	github.com/stretchr/testify v1.7.0
 )
